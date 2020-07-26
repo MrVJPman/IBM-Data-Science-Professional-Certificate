3cbb12c0aa78eafc0b12ce3a2f9932439fafc57f
mrvjpman <bob.kong@mail.utoronto.ca>
2020-07-26 15:16:14 -0400

    functions exercise

---
+++ Course 4-Python for Data Science and AI/Week 3/functions.py
@@ -0,0 +1,24 @@
+#Come up with a function that divides the first input by the second input:
+
+def function_one(a, b):
+    return a / b
+
+#Use the function con for the following question
+
+def con(a, b):
+    return(a + b)
+
+#Can the con function we defined before be used to add to integers or strings?
+
+#Answer : Both integers and strings
+
+con(1, 2)
+con("a", "b")
+
+#Can the con function we defined before be used to concentrate a list or tuple?
+
+#Answer : Both Lists and Tuples
+
+
+con([1], [2])
+con((1), (2))
