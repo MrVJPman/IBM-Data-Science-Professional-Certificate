d3a5322ca7b63b1d70b25d527ce02cfbfc705168
mrvjpman <bob.kong@mail.utoronto.ca>
2020-07-25 21:27:07 -0400

    Dictionaries exercise

---
+++ Course 4-Python for Data Science and AI/Week 2/Dictionaries.py
@@ -0,0 +1,30 @@
+# Question sample dictionary
+
+soundtrack_dic = {"The Bodyguard":"1992", "Saturday Night Fever":"1977"}
+soundtrack_dic 
+
+#In the dictionary soundtrack_dict what are the keys ?
+
+soundtrack_dic.keys()
+
+# In the dictionary soundtrack_dict what are the values ?
+
+soundtrack_dic.values()
+
+#The Albums Back in Black, The Bodyguard and Thriller have the following music recording sales in millions 50, 50 and 65 respectively:
+
+#a) Create a dictionary album_sales_dict where the keys are the album name and the sales in millions are the values.
+
+album_sales_dict = {"Back in Black" : 50, "The Bodyguard" : 50, "Thriller" :65}
+
+#b) Use the dictionary to find the total sales of Thriller:
+
+album_sales_dict["Thriller"]
+
+#c) Find the names of the albums from the dictionary using the method keys:
+
+album_sales_dict.keys()
+
+#d) Find the names of the recording sales from the dictionary using the method values:
+
+album_sales_dict.values()
